238c12a4780fcc17885c5711c89bfdf457c7a027
aseem <sushmaacharya984@gmail.com>
2024-04-10 10:30:51 +0545

    haha4

--- linkedlist.h
+++ linkedlist.h
@@ -1,23 +1,25 @@
-#include "linkedlist.h"
+#pragma once
 class node{
     public:
     int info;
-    node*next;
+    node* next;
     node(){}
     node(int d):info(d),next( nullptr){}
     node(int d,node*next):info (d),next(next){}
     };
 class linkedlist{
     private:
-    node HEAD;
-    node TAIL;
+    node* HEAD;
+    node* TAIL;
     public:
     linkedlist();
     ~linkedlist();
+
     void add(node *pred,int data);
     void addtohead(int data);
     void removefromtail(int data);
     bool remove (int data);
+
     bool removefromhead(int &data);
     bool removefromtail(int &data);
     bool isempty();
