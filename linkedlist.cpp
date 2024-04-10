1cd9856a3252202ea4640771b58ed2b141d5237f
aseem <kcsakshi92@gmail.com>
2024-04-10 09:47:32 +0545

    haha

---
+++ linkedlist.cpp
@@ -0,0 +1,10 @@
+#include 
+class node{
+    public:
+    int info;
+    Node*next;
+    Node(){}
+    Node(int d):info(d),next(NULL)){}
+    Node(int d,Node*next:info (d),next(NULL)){}
+    };
+class LinkedList
